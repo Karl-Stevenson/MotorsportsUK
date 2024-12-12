cdeebc06be40a48f5b44435ec6076cb8e22368c0
aprna-ops <50088841@belfastmet.ac.uk>
2024-12-12 10:17:25 +0000

    commented down some of the not useful codes

--- met-2024-student-django-react-main/met-2024-student-django-react-main/collegeapi/api/models.py
+++ met-2024-student-django-react-main/met-2024-student-django-react-main/collegeapi/api/models.py
@@ -1,32 +1,4 @@
 from django.db import models
-
-# Creating models here.
-class Lecturer(models.Model): 
-    first_name = models.CharField(max_length=100) 
-    last_name = models.CharField(max_length=100) 
-    email = models.EmailField() 
-    staff_number = models.CharField(max_length=10) 
-
-    def __str__(self): 
-        return f"{self.first_name} {self.last_name}" 
-
-class Student(models.Model): 
-    first_name = models.CharField(max_length=100) 
-    last_name = models.CharField(max_length=100) 
-    email = models.EmailField() 
-    student_number = models.CharField(max_length=10) 
-
-    def __str__(self): 
-        return f"{self.first_name} {self.last_name}"
-    
-class Course(models.Model): 
-    title = models.CharField(max_length=200) 
-    description = models.TextField() 
-    lecturer = models.ForeignKey(Lecturer, on_delete=models.SET_NULL, null=True, blank=True) 
-    students = models.ManyToManyField(Student, blank=True) 
-
-    def __str__(self): 
-        return self.title
     
     
 # Model for Sales team
@@ -109,3 +81,32 @@ class Opportunity(models.Model):
  
     def __str__(self):
         return f"Opportunity: {self.description} ({self.stage})"
+
+
+# Creating models here.
+class Lecturer(models.Model): 
+    first_name = models.CharField(max_length=100) 
+    last_name = models.CharField(max_length=100) 
+    email = models.EmailField() 
+    staff_number = models.CharField(max_length=10) 
+
+    def __str__(self): 
+        return f"{self.first_name} {self.last_name}" 
+
+class Student(models.Model): 
+    first_name = models.CharField(max_length=100) 
+    last_name = models.CharField(max_length=100) 
+    email = models.EmailField() 
+    student_number = models.CharField(max_length=10) 
+
+    def __str__(self): 
+        return f"{self.first_name} {self.last_name}"
+    
+class Course(models.Model): 
+    title = models.CharField(max_length=200) 
+    description = models.TextField() 
+    lecturer = models.ForeignKey(Lecturer, on_delete=models.SET_NULL, null=True, blank=True) 
+    students = models.ManyToManyField(Student, blank=True) 
+
+    def __str__(self): 
+        return self.title
