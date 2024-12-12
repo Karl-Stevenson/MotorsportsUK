0fc2812c88b2a0cc4e2ba7d04faeb097c1aec608
Prakash Chakraborty <50105073@belfastmet.ac.uk>
2024-12-12 12:32:41 +0000

    Form Validation & Duplicate in Locations

--- met-2024-student-django-react-main/met-2024-student-django-react-main/college-react/src/motorparts/CreateLocation.js
+++ met-2024-student-django-react-main/met-2024-student-django-react-main/college-react/src/motorparts/CreateLocation.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 
 export default function App() {
@@ -36,6 +36,46 @@ export default function App() {
             .catch((e) => console.log(e));
     };
 
+    const [locations, setLocations] = useState([])
+
+    useEffect(() => {
+
+        const requestOptions = {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json"
+            }
+        };
+
+        fetch('/api/locations/', requestOptions)
+            .then((response) => response.json())
+            .then((data) => setLocations(data))
+            //.then((data) => console.log(data))
+            .catch((e) => console.log(e));
+    }, []);
+
+    const validateAddress = (data) => {
+        let element = document.getElementById("address");
+        if (locations.map((a) => a.address).indexOf(data) >= 0) {
+            element.setCustomValidity("Duplicate Address");
+            element.reportValidity();
+        } else {
+            element.setCustomValidity("");
+        }
+        setAddress(data)
+    }
+
+    const validatePhone = (data) => {
+        let element = document.getElementById("phone");
+        if (locations.map((a) => a.phone).indexOf(data) >= 0) {
+            element.setCustomValidity("Duplicate Phone Number");
+            element.reportValidity();
+        } else {
+            element.setCustomValidity("");
+        }
+        setPhone(data)
+    }
+
     return (
         <center>
             <div className="text-5xl font-bold p-3">
@@ -44,45 +84,59 @@ export default function App() {
                 </span>
             </div>
             {!status ?
-                <form className='w-full max-w-lg' onSubmit={handleSubmit}>
+                <form className='w-full max-w-lg group' onSubmit={handleSubmit} noValidate>
                     <div class="flex flex-wrap -mx-3 mb-6">
-                    <div class="w-full px-3">
-                        <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-zip">
-                            Address
-                        </label>
-                        <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-                            id="grid-zip" type="text" placeholder="Newtownabbey, Belfast"
-                            value={address}
-                            onChange={(e) => setAddress(e.target.value)} />
-                    </div>
-                    </div>
-                    <div class="flex flex-wrap -mx-3 mb-6">
-                    <div class="w-full  px-3">
-                        <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-last-name">
-                            Phone
-                        </label>
-                        <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-                            id="grid-last-name" type="tel" placeholder="7XXXXXXXXX"
-                            value={phone}
-                            onChange={(e) => setPhone(e.target.value)} />
-                    </div>
+                        <div class="w-full px-3">
+                            <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="address">
+                                Address
+                            </label>
+                            <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500 invalid:[&:not(:placeholder-shown):not(:focus)]:border-red-500 peer"
+                                id="address" type="text" placeholder="Newtownabbey, Belfast"
+                                value={address}
+                                onChange={(e) => validateAddress(e.target.value)}
+                                required />
+                            <span class="mt-2 hidden text-sm text-red-500 peer-[&:not(:placeholder-shown):not(:focus):invalid]:block">
+                                Please enter a valid address
+                            </span>
+                        </div>
                     </div>
                     <div class="flex flex-wrap -mx-3 mb-6">
-                    <div class="w-full px-3">
-                        <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-password">
-                            Manager
-                        </label>
-                        <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-                            id="grid-password" type="text" placeholder="John Doe"
-                            value={manager}
-                            onChange={(e) => setManager(e.target.value)} />
+                        <div class="w-full px-3">
+                            <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="phone">
+                                Phone
+                            </label>
+                            <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500 invalid:[&:not(:placeholder-shown):not(:focus)]:border-red-500 peer"
+                                id="phone" type="text" placeholder="7XXXXXXXXX"
+                                value={phone}
+                                onChange={(e) => validatePhone(e.target.value)}
+                                required
+                                pattern="^(?:0|\+?44)(?:\d\s?){9,10}$" />
+                            <span class="mt-2 hidden text-sm text-red-500 peer-[&:not(:placeholder-shown):not(:focus):invalid]:block">
+                                Please enter a valid UK phone number
+                            </span>
+
+                        </div>
                     </div>
+                    <div class="flex flex-wrap -mx-3 mb-6">
+                        <div class="w-full px-3">
+                            <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-password">
+                                Manager
+                            </label>
+                            <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500 invalid:[&:not(:placeholder-shown):not(:focus)]:border-red-500 peer"
+                                id="grid-password" type="text" placeholder="John Doe"
+                                value={manager}
+                                onChange={(e) => setManager(e.target.value)}
+                                required />
+                            <span class="mt-2 hidden text-sm text-red-500 peer-[&:not(:placeholder-shown):not(:focus):invalid]:block">
+                                Please enter a manager
+                            </span>
+                        </div>
                     </div>
                     <div class="flex flex-wrap -mx-3 mb-6 content-center px-2">
-                        <Link to='/salesreps' class="m-1 bg-transparent hover:bg-slate-900 text-slate-700 font-semibold hover:text-white py-2 px-4 border border-slate-700 hover:border-transparent rounded">
+                        <Link to={'/customers/' + customerId + '/'} class="m-1 bg-transparent hover:bg-slate-900 text-slate-700 font-semibold hover:text-white py-2 px-4 border border-slate-700 hover:border-transparent rounded">
                             Go Back
                         </Link>
-                        <button class="m-1 bg-transparent hover:bg-green-700 text-green-700 font-semibold hover:text-white py-2 px-4 border border-green-700 hover:border-transparent rounded" type='submit'>
+                        <button class="m-1 bg-transparent hover:bg-green-700 text-green-700 font-semibold hover:text-white py-2 px-4 border border-green-700 hover:border-transparent rounded group-invalid:pointer-events-none group-invalid:opacity-30" type='submit'>
                             Add
                         </button>
                     </div>
@@ -91,15 +145,15 @@ export default function App() {
                 <div className='w-full max-w-lg'>
                     <div className="text-3xl font-bold p-3">Location Added &#x2705;</div>
                     <div class="mx-3 mb-6 content-center p-3">
-                        <Link to={'/customers/'+ customerId + '/' } class="m-1 bg-transparent hover:bg-slate-900 text-slate-700 font-semibold hover:text-white py-2 px-4 border border-slate-700 hover:border-transparent rounded">
+                        <Link to={'/customers/' + customerId + '/'} class="m-1 bg-transparent hover:bg-slate-900 text-slate-700 font-semibold hover:text-white py-2 px-4 border border-slate-700 hover:border-transparent rounded">
                             Go Back to List
                         </Link>
-                        <Link to={()=>window.location.reload()} class="m-1 bg-transparent hover:bg-green-700 text-green-700 font-semibold hover:text-white py-2 px-4 border border-green-700 hover:border-transparent rounded" type='submit'>
+                        <Link to={() => window.location.reload()} class="m-1 bg-transparent hover:bg-green-700 text-green-700 font-semibold hover:text-white py-2 px-4 border border-green-700 hover:border-transparent rounded">
                             Add More
                         </Link>
                     </div>
                 </div>
-                
+
             }
         </center>
     )
